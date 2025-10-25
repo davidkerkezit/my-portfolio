19d6aeb0166d0f23b203bee987ab999091ff3751
David Kerkez <davidkerkez@gmail.com>
2025-10-26 00:07:31 +0200

    CV update

--- src/components/Header/Content.jsx
+++ src/components/Header/Content.jsx
@@ -17,8 +17,8 @@ const HighlightText = tw.p`uppercase lg:text-base  text-[14px] font-light leadin
 function Content({ contactRef }) {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/DavidCV2025.pdf";
-    link.download = "DavidCV2025.pdf";
+    link.href = "/DavidCV2025v2.pdf";
+    link.download = "DavidCV2025v2.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
